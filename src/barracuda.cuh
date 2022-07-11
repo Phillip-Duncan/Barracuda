ddbd47f000a8e7ba04a218221b9bf7ca731f6f9a
Phillip Duncan <60393535+Phillip-Duncan@users.noreply.github.com>
2022-07-11 19:51:23 +1200

    Instructions performance tweaks
    1. Switched to switch statement for instruction eval.
    
    2. Combination instruction performance problems with integration,
    due to aggressive expansion of integrate/romberg code in assembly.
    Have temporarily removed usage whilst working on a potential solution.

--- src/barracuda.cuh
+++ src/barracuda.cuh
@@ -18,7 +18,7 @@
 #endif
 
 #ifndef MSTACK_SPECIALS
-#define MSTACK_SPECIALS 1
+#define MSTACK_SPECIALS 0
 #endif
 
 #ifndef MSTACK_LOOP_NEST_LIMIT
@@ -42,90 +42,99 @@ L tid, I nt, Vars<F> &variables, I* loop_stack, I &loop_idx )
     long long op = pop(opstack,opstackidx);
     F value = pop(valuestack, valuestackidx);
 
-    // Is an ordinary operation
-    if (type==0) {
-        //op = pop(opstack,opstackidx);
-        operation<F>(op, outputstack, outputstackidx, nt, 0, variables);
-        return;
-    }
-    // Is a value
-    else if (type==1) {
-        //value = pop(valuestack, valuestackidx);
-        push_t(outputstack, outputstackidx, value, nt);
-        return;
-    }
-    // JUMP/Goto statement
-    else if (type==2) {
-        value = pop_t(outputstack, outputstackidx, nt);
-        jmp(stack, stackidx, stacksize, opstackidx, valuestackidx, (I)value);
-    }
-    // JUMP IF == 0 / conditional goto statement
-    else if (type==3) {
-        value = pop_t(outputstack, outputstackidx, nt);
-        I cond = (I)pop_t(outputstack, outputstackidx, nt);
-        if (cond==0) {
+    switch(type) {
+        case 0: 
+        {
+            operation<F>(op, outputstack, outputstackidx, nt, 0, variables);
+            break;
+        }
+        case 1:
+        {
+            push_t(outputstack, outputstackidx, value, nt);
+            break;
+        }
+        case 2: 
+        {
+            value = pop_t(outputstack, outputstackidx, nt);
             jmp(stack, stackidx, stacksize, opstackidx, valuestackidx, (I)value);
+            break;
         }
-        else {
+        case 3:
+        {
+            value = pop_t(outputstack, outputstackidx, nt);
+            I cond = (I)pop_t(outputstack, outputstackidx, nt);
+            if (cond==0) {
+                jmp(stack, stackidx, stacksize, opstackidx, valuestackidx, (I)value);
+            }
+            else {
+            }
+            break;
         }
-    }
-    // Combinational instructions.
-    else if (type==4) {
-        push_t(outputstack, outputstackidx, value, nt);
-        operation<F>(op, outputstack, outputstackidx, nt, 0, variables);
-        return;
-    }
-    else if (type==5) {
-        operation<F>(op, outputstack, outputstackidx, nt, 0, variables);
-        push_t(outputstack, outputstackidx, value, nt);
-        return;
-    }
-    // function pointer operation
-    #if MSTACK_UNSAFE==1
-        else if (type<0) {
-            op = pop(opstack, opstackidx);
-            operation<F>(type, op, outputstack, outputstackidx, nt, 0, variables);
-            return;
+        case 4:
+        {
+            push_t(outputstack, outputstackidx, value, nt);
+            operation(op, outputstack, outputstackidx, nt, 0, variables);
+            break;
         }
-    #endif
-    else if (type==99) {
-        I imax = (I)pop_t(outputstack, outputstackidx, nt);
-        I i = (I)pop_t(outputstack, outputstackidx, nt);
-        // Store stack locations of loop entry, i and imax.
-        loop_stack[5*loop_idx] = stackidx;
-        loop_stack[5*loop_idx+1] = opstackidx;
-        loop_stack[5*loop_idx+2] = valuestackidx;
-        loop_stack[5*loop_idx+3] = i;
-        loop_stack[5*loop_idx+4] = imax;
-        loop_idx = loop_idx + 1;
-        return;
-    }
-    else if (type==100) {
-        // Compare values of i,imax to see whether loop has ended or continues
-        I i = loop_stack[5*(loop_idx-1)+3];
-        I imax = loop_stack[5*(loop_idx-1)+4];
-        i = i + 1;
-        loop_stack[5*(loop_idx-1)+3] = i;
-        // If loop has reached end decrement loop_idx and return
-        if (i>=imax){
-            loop_idx = loop_idx - 1;
-            return;
+        case 5: 
+        {
+            operation(op, outputstack, outputstackidx, nt, 0, variables);
+            push_t(outputstack, outputstackidx, value, nt);
+            break;
         }
-        // Otherwise goto start of loop.
-        else {
-            // Reset program counter
-            //variables.PC = variables.PC + stackidx - loop_stack[5*(loop_idx-1)];
-            variables.PC = stacksize - loop_stack[5*(loop_idx-1)];
+        case 99: 
+        {
+            I imax = (I)pop_t(outputstack, outputstackidx, nt);
+            I i = (I)pop_t(outputstack, outputstackidx, nt);
+            // Store stack locations of loop entry, i and imax.
+            loop_stack[5*loop_idx] = stackidx;
+            loop_stack[5*loop_idx+1] = opstackidx;
+            loop_stack[5*loop_idx+2] = valuestackidx;
+            loop_stack[5*loop_idx+3] = i;
+            loop_stack[5*loop_idx+4] = imax;
+            loop_idx = loop_idx + 1;
+            break;
+        }
+        case 100:
+        {
+            // Compare values of i,imax to see whether loop has ended or continues
+            I i = loop_stack[5*(loop_idx-1)+3];
+            I imax = loop_stack[5*(loop_idx-1)+4];
+            i = i + 1;
+            loop_stack[5*(loop_idx-1)+3] = i;
+            // If loop has reached end decrement loop_idx and return
+            if (i>=imax){
+                loop_idx = loop_idx - 1;
+            }
+            // Otherwise goto start of loop.
+            else {
+                // Reset program counter
+                //variables.PC = variables.PC + stackidx - loop_stack[5*(loop_idx-1)];
+                variables.PC = stacksize - loop_stack[5*(loop_idx-1)];
 
-            // Swap indicies/sizes
-            stackidx = loop_stack[5*(loop_idx-1)];
-            //stacksize = loop_stack[5*(loop_idx-1)];
-            opstackidx = loop_stack[5*(loop_idx-1) + 1];
-            //opstacksize = loop_stack[5*(loop_idx-1) + 1];
-            valuestackidx = loop_stack[5*(loop_idx-1) + 2];
-            //valuestacksize = loop_stack[5*(loop_idx-1) + 2];
-            return;
+                // Swap indicies/sizes
+                stackidx = loop_stack[5*(loop_idx-1)];
+                //stacksize = loop_stack[5*(loop_idx-1)];
+                opstackidx = loop_stack[5*(loop_idx-1) + 1];
+                //opstacksize = loop_stack[5*(loop_idx-1) + 1];
+                valuestackidx = loop_stack[5*(loop_idx-1) + 2];
+                //valuestacksize = loop_stack[5*(loop_idx-1) + 2];
+            }
+            break;
         }
+        default:
+        {
+            // function pointer operation
+            #if MSTACK_UNSAFE==1
+                if (type<0) {
+                    op = pop(opstack, opstackidx);
+                    operation<F>(type, op, outputstack, outputstackidx, nt, 0, variables);
+                    break;
+                }
+            #endif
+            break;
+        }
+
     }
 }
 
